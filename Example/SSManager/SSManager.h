8c2bcaf0193e977a0624e2fdc89b2407b12955a7
Benedict Cumberbatch <>
2018-12-27 15:41:07 +0800

    dasdas.asdasdasd

---
+++ Example/SSManager/SSManager.h
@@ -0,0 +1,19 @@
+//
+//  SSManager.h
+//  SSManager
+//
+//  Created by javis on 2018/12/27.
+//  Copyright © 2018 SpikeLeeHom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSManager.
+FOUNDATION_EXPORT double SSManagerVersionNumber;
+
+//! Project version string for SSManager.
+FOUNDATION_EXPORT const unsigned char SSManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSManager/PublicHeader.h>
+
+
